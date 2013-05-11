b2e15bc35a0a4d693d14d11dc80d36dd4ef78fe0
Vitaly Aminev <v@aminev.me>
2013-05-11 23:08:56 +0400

    added package name

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "nano"
 maintainer       "Joseph Anthony Pasquale Holsten"
 maintainer_email "joseph@josephholsten.com"
 license          "Apache 2.0"
 description      "Installs nano"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 %w{debian ubuntu arch redhat centos fedora scientific}.each do |os|
   supports os
